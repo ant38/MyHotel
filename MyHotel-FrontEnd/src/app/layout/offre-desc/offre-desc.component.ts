6c4f2a22cdc50b68bd7affbd1c0d422439ca5d15
alice2909 <alice.rivoal@gmail.com>
2017-12-16 16:56:40 +0100

    offre desc un peu améliorée

--- MyHotel-FrontEnd/src/app/layout/offre-desc/offre-desc.component.ts
+++ MyHotel-FrontEnd/src/app/layout/offre-desc/offre-desc.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Offer } from '../../_models/index';
+import { Offer, Hotel, Room } from '../../_models/index';
 
 @Component({
   selector: 'app-offre-desc',
