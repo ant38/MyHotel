86061b0b7b7d2864d93ea0a9173d6c14de433a1c
s6mon <sim.chambonnet@gmail.com>
2017-12-16 16:54:06 +0100

    merge erreur corrigée

--- MyHotel-FrontEnd/src/app/filtres/filtre-lieu/filtre-lieu.component.ts
+++ MyHotel-FrontEnd/src/app/filtres/filtre-lieu/filtre-lieu.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component, OnInit, Injectable } from '@angular/core';
-=======
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
->>>>>>> c7d1969df2d44346d61dba9d385895e984687c2d
+import { Component, OnInit, Output, EventEmitter, Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-filtre-lieu',
@@ -14,24 +10,14 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class FiltreLieuComponent implements OnInit {
 
   lieuVar: string;
-  @Output() 
-  lieuChange: EventEmitter<string> = new EventEmitter<string>();
+  @Output() lieuChange: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
-<<<<<<< HEAD
-  getLieu(): string {
-    return this.lieuVar;
-  }
-  onKeyLieu(e){
-    console.log(this.lieuVar);
-  } 
-=======
   onChangeLieu(e){
     //console.log(this.lieuVar);
     this.lieuChange.emit(this.lieuVar);
   }    
->>>>>>> c7d1969df2d44346d61dba9d385895e984687c2d
 
   ngOnInit() {  }
 
