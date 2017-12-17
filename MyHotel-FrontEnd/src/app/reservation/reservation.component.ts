4d0f2834feb109b9fa665c8dcc23501d4ecf0c8f
s6mon <sim.chambonnet@gmail.com>
2017-12-17 17:56:48 +0100

    réservation

--- MyHotel-FrontEnd/src/app/reservation/reservation.component.ts
+++ MyHotel-FrontEnd/src/app/reservation/reservation.component.ts
@@ -46,20 +46,17 @@ export class ReservationComponent {
 
   openCheckout() {
     this.prix = this.offre.price;
-    console.log(this.prix);
     var handler = (<any>window).StripeCheckout.configure({
       key: 'pk_test_oi0sKPJYLGjdvOXOM8tE8cMa',
       locale: 'auto',
       token: function (token: any) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
       }
     });
 
     handler.open({
       name: 'Paiement par carte',
       description: 'saisir votre email:',
-      amount: this.prix*100
+      amount: this.prix*100 //le prix est divisé par 100 !?
     });
   }
 }
